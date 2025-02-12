e975bf8b2dcee00627ddf57e57fd2ba6b0dbaf7b
Frank Lan <frank.lan@rea-group.com>
2025-02-12 12:09:06 +1100

    3

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,10 +7,8 @@ docker run --rm -it -v "$(pwd)":/home/rust/src messense/rust-musl-cross:aarch64-
 docker run --rm -it -v "$(pwd)":/home/rust/src messense/rust-musl-cross:armv7-musleabi cargo build --release
 docker run --rm -it -v "$(pwd)":/home/rust/src messense/rust-musl-cross:x86_64-musl cargo build --release
 
-rm -vrf build
-mkdir build
-cp -v target/aarch64-unknown-linux-musl/release/dynamic-ip-handler build/dynamic-ip-handler-aarch64
-cp -v target/armv7-unknown-linux-musleabi/release/dynamic-ip-handler build/dynamic-ip-handler-armv7
-cp -v target/x86_64-unknown-linux-musl/release/dynamic-ip-handler build/dynamic-ip-handler-x86_64
+cp -v target/aarch64-unknown-linux-musl/release/dynamic-ip-handler dynamic-ip-handler-aarch64
+cp -v target/armv7-unknown-linux-musleabi/release/dynamic-ip-handler dynamic-ip-handler-armv7
+cp -v target/x86_64-unknown-linux-musl/release/dynamic-ip-handler dynamic-ip-handler-x86_64
 
 rm -vrf target
