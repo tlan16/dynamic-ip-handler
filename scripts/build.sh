ec24acec85a528e251708c74376b012f1314ce12
Frank Lan <frank.lan@rea-group.com>
2025-03-07 10:42:57 +1100

    improve build size

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,4 +11,8 @@ cp -v target/aarch64-unknown-linux-musl/release/dynamic-ip-handler dynamic-ip-ha
 cp -v target/armv7-unknown-linux-musleabi/release/dynamic-ip-handler dynamic-ip-handler-armv7
 cp -v target/x86_64-unknown-linux-musl/release/dynamic-ip-handler dynamic-ip-handler-x86_64
 
+upx --best --lzma dynamic-ip-handler-aarch64
+upx --best --lzma dynamic-ip-handler-armv7
+upx --best --lzma dynamic-ip-handler-x86_64
+
 rm -vrf target
